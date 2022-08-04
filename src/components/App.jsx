ae1c3f9d11faf615ecde07d1fa1d86fb114a9971
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-08-04 15:13:51 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,6 +37,10 @@ export class App extends Component {
 
       this.renderGallery();
     }
+
+    if (nextPage > 1) {
+      this.scroll();
+    }
   }
 
   renderGallery = () => {
@@ -55,8 +59,9 @@ export class App extends Component {
               'Sorry, there are no images matching your search query. Please try again.',
           });
         }
-        this.scroll();
+        // this.scroll();
       })
+
       .catch(error => this.setState({ error, status: 'rejected' }));
   };
 
@@ -108,7 +113,9 @@ export class App extends Component {
             <HelloMessage>What do you want to look for?</HelloMessage>
           )}
 
-          {images.length !== 0 && <Button onClick={this.loadMore} />}
+          {images.length !== 0 && status === 'resolved' && (
+            <Button onClick={this.loadMore} />
+          )}
 
           {showModal && (
             <Modal onClose={this.toggleModal}>
